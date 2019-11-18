3a9c170742c0fac27a91de90913fec14764c3a54
so_young <qksthdhd94@gmail.com>
2019-11-18 20:52:47 +0900

    만들어둔 ContextUtil 활용 예제

--- app/src/main/java/com/example/a20191118_01_getmethodpractice/LoginActivity.kt
+++ app/src/main/java/com/example/a20191118_01_getmethodpractice/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.a20191118_01_getmethodpractice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.example.a20191118_01_getmethodpractice.utils.ContextUtil
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
@@ -18,8 +19,8 @@ class LoginActivity : BaseActivity() {
 
         loginBtn.setOnClickListener {
             if(rememberIdCheckBox.isChecked){
-                Toast.makeText(mContext,"아이디 저장 필요",Toast.LENGTH_SHORT).show()
-
+                ContextUtil.setUserId(mContext,idEdt.text.toString())
+                Toast.makeText(mContext,"아이디 저장 했습니다.",Toast.LENGTH_SHORT).show()
             }else{
                 Toast.makeText(mContext,"아이디 저장 불필요",Toast.LENGTH_SHORT).show()
             }
@@ -29,6 +30,11 @@ class LoginActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        //저장되어 있는 아이디가 뭔지?
+        var savedID = ContextUtil.getUserId(mContext)
+
+        idEdt.setText(savedID)
     }
 
 }
