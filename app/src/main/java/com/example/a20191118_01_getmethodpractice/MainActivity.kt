97be6eb722a158be18cbcf55dff22894421313fc
so_young <qksthdhd94@gmail.com>
2019-11-18 22:18:08 +0900

    내 정보 가져오기 기능 구현 및 사용자 정보 파싱 => User 클래스가 전담

--- app/src/main/java/com/example/a20191118_01_getmethodpractice/MainActivity.kt
+++ app/src/main/java/com/example/a20191118_01_getmethodpractice/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.a20191118_01_getmethodpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.a20191118_01_getmethodpractice.datas.User
+import com.example.a20191118_01_getmethodpractice.utils.ServerUtil
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -15,5 +20,20 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        ServerUtil.getRequestMyInfo(mContext,object : ServerUtil.JsonResponseHandler{
+            override fun onResponse(json: JSONObject) {
+                val data = json.getJSONObject("data")
+                val user = data.getJSONObject("user")
+
+                var me = User.getUserFromJson(user)
+
+                runOnUiThread {
+                    nameTxt.text = me.name
+                    phoneNumTxt.text = me.phone
+                    loginIdTxt.text = me.login_id
+                }
+            }
+
+        })
     }
 }
--- app/src/main/java/com/example/a20191118_01_getmethodpractice/utils/ServerUtil.kt
+++ app/src/main/java/com/example/a20191118_01_getmethodpractice/utils/ServerUtil.kt
@@ -8,31 +8,35 @@ import java.io.IOException
 
 class ServerUtil {
 
-
-    interface JsonResponseHandler{
-        fun onResponse(json:JSONObject)
+    interface JsonResponseHandler {
+        fun onResponse(json: JSONObject)
     }
 
-    companion object{
+    companion object {
 
         var BASE_URL = "http://192.168.0.26:5000"
 
-        fun postRequestLogin(context: Context, loginId:String, loginPw:String, handler:JsonResponseHandler?){
+        fun postRequestLogin(
+            context: Context,
+            loginId: String,
+            loginPw: String,
+            handler: JsonResponseHandler?
+        ) {
 
-            //우리가 만든 앱을 클라이언트 역할로 동작하게 해주는 클래스
+//            우리가 만드는 앱을 클라이언트 역할로 동작하게 해주는 클래스
             var client = OkHttpClient()
 
-            //기능 주소와 서버 주소를 조합해서 실제 요청 주소 완성
-            var url ="${BASE_URL}/auth"
+//            기능 주소와 서버 주소를 조합해서 실제 요청 주소 완성
+            var url = "${BASE_URL}/auth"
 
-
-            //POST 메소드에서 요구하는 파리멑를 FormBody에 담아줌.
+//            POST 메쏘드에서 요구하는 파라미터를 FormBody에 담아줌
             var formBody = FormBody.Builder()
-                .add("login_id",loginId)
-                .add("password",loginPw)
+                .add("login_id", loginId)
+                .add("password", loginPw)
                 .build()
 
-            //실제로 날아갈 요청(request)을 생성
+//            실제로 날아갈 요청(request)을 생성.
+
             var request = Request.Builder()
                 .url(url)
                 .post(formBody)
@@ -40,14 +44,48 @@ class ServerUtil {
 
             client.newCall(request).enqueue(object : Callback {
                 override fun onFailure(call: Call, e: IOException) {
-                    Log.e("서버통신 에러",e.localizedMessage)
+
+                    Log.e("서버통신에러", e.localizedMessage)
                 }
 
                 override fun onResponse(call: Call, response: Response) {
-                    var body = response.body!!.string()
-                    var json: JSONObject = JSONObject(body)
 
+                    var body = response.body()!!.string()
+                    var json = JSONObject(body)
                     handler?.onResponse(json)
+                }
+
+            })
+        }
+
+        fun getRequestMyInfo(context: Context, handler: JsonResponseHandler?) {
+
+            var client = OkHttpClient()
+
+            var urlBuilder = HttpUrl.parse("${BASE_URL}/my_info")!!.newBuilder()
+//            GET 방식의 파라미터를 첨부하는 방법
+            urlBuilder.addEncodedQueryParameter("device_token", "test")
+
+//            url 최종 확정
+            val requestUrl = urlBuilder.build().toString()
+            Log.d("요청URL", requestUrl)
+
+            val request = Request.Builder()
+                .url(requestUrl)
+                .header("X-Http-Token", ContextUtil.getUserToken(context))
+                .build()
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    Log.e("서버통신에러", e.localizedMessage)
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+
+                    val body = response.body()!!.string()
+                    val json = JSONObject(body)
+                    handler?.onResponse(json)
+
                 }
 
             })
@@ -56,4 +94,5 @@ class ServerUtil {
 
 
     }
+
 }
