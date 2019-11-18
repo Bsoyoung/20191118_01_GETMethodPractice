75add8e4d0ec5d00a88b72d318a2d540df463810
so_young <qksthdhd94@gmail.com>
2019-11-18 19:59:27 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a20191118_01_getmethodpractice/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.example.a20191118_01_getmethodpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+
+}
